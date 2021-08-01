13cafece097e8d1767347ee539b92cbba5327848
S.A.M <alexdavid486@gmail.com>
2021-08-01 14:30:59 +0200

    Fixing the mobile version

--- Autumn design/js/main.js
+++ Autumn design/js/main.js
@@ -1,8 +1,14 @@
 const slideMenu = document.getElementById("icon");
 const links = document.getElementById("links")
+const slideLinks = document.querySelectorAll(".slideMenu ul li");
 
 slideMenu.addEventListener("click", () => {
     document.body.classList.toggle("active");
     links.style.transition = "all 0.3s ease-in"
 });
+slideLinks.forEach((link) => {
+    link.addEventListener("click" , () => {
+        document.body.classList.remove("active");
+    })
+})
 

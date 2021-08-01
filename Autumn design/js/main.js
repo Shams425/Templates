c3365593297edd542cf5f52e5e4007e5f5f38a6d
S.A.M <alexdavid486@gmail.com>
2021-08-01 14:49:41 +0200

    improve the code the add remove menu on screen click

--- Autumn design/js/main.js
+++ Autumn design/js/main.js
@@ -1,14 +1,23 @@
-const slideMenu = document.getElementById("icon");
-const links = document.getElementById("links")
+const slideMenu = document.querySelectorAll(".icon span");
+const links = document.getElementById("links");
 const slideLinks = document.querySelectorAll(".slideMenu ul li");
+const icon = document.getElementById("icon");
 
-slideMenu.addEventListener("click", () => {
+slideMenu.forEach((slide) => {
+    slide.addEventListener("click", () => {
     document.body.classList.toggle("active");
     links.style.transition = "all 0.3s ease-in"
-});
+    });
+})
+
 slideLinks.forEach((link) => {
     link.addEventListener("click" , () => {
         document.body.classList.remove("active");
     })
 })
+window.onclick = (el) => {
+    if (!el.target.matches(".slideMenu") && (!el.target.matches(".icon")) && !el.target.matches(".icon span")) {
+        document.body.classList.remove("active");
+    }
+}
 

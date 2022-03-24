2bdc752b25cca8bfb5fb71fcfca28cb8940f3117
Shams425 <alexdavid486@gmail.com>
2022-03-24 23:50:19 +0200

    The new Projects

---
+++ Project 2/assets/js/main.js
@@ -0,0 +1,41 @@
+$(function () {
+  $(".commentsContainer").owlCarousel({
+    loop: true,
+    center: true,
+    autoPlay: true,
+    autoplayHoverPause: true,
+    margin: 30,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      770: {
+        items: 2,
+      },
+    },
+  });
+
+  AOS.init({
+    duration: 800,
+    anchorPlacement: "center-bottom",
+  });
+});
+
+//show and hide text in about section
+const titles = document.querySelectorAll("#history .bottom .titles .title");
+const texts = document.querySelectorAll("#history .bottom .info");
+
+titles.forEach((title) => {
+  title.addEventListener("click", function (e) {
+    titles.forEach((title) => {
+      title.classList.remove("active");
+    });
+    this.classList.add("active");
+    texts.forEach((text) => {
+      text.classList.remove("show");
+    });
+    document.querySelector(title.dataset.target).classList.add("show");
+  });
+});
+
+console.log(titles);
